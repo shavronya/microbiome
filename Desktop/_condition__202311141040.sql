fe2d0c0a05c9f8cd6a5d3414c69ab2b47feaf967
darias <shavronya@gmail.com>
2023-11-14 10:56:17 +0800

    Initial commit

---
+++ Desktop/_condition__202311141040.sql
@@ -0,0 +1,33 @@
+INSERT INTO microbiome.`condition` (condition_id,condition_type,complication_or_side_disease,abbreviation) VALUES
+	 (1,'Colorectal cancer',NULL,'CRC'),
+	 (2,'Systemic inflammatory response syndrome',NULL,'SIRS'),
+	 (3,'Type 2 diabetes',NULL,'T2D'),
+	 (4,'Diabetic retinopathy','Type 2 diabetes','DR and T2D'),
+	 (5,'Hepatocellular Carcinoma',NULL,'HC'),
+	 (6,'Obesity',NULL,'OB'),
+	 (7,'Metabolic‐associated fatty liver disease',NULL,'MAFLD'),
+	 (8,'Human immunodeficiency virus-1 infection',NULL,'HIV'),
+	 (9,'Crohn’s disease',NULL,'CD'),
+	 (10,'Functional gastrointestinal disorders ',NULL,'FGIDs');
+INSERT INTO microbiome.`condition` (condition_id,condition_type,complication_or_side_disease,abbreviation) VALUES
+	 (11,'Congenital heart disease followed',NULL,'CHDs'),
+	 (12,'Esophageal adenocarcinoma ','Barrett''s esophagus','EAC'),
+	 (13,'Breast cancer',NULL,'BC'),
+	 (14,'Hidradenitis suppurativa',NULL,'HS'),
+	 (15,'Ulcerative colitis',NULL,'UC'),
+	 (16,'Ischemic colitis',NULL,'IC'),
+	 (17,'Alzheimer’s disease',NULL,'AD'),
+	 (18,'Subclinical Carotid Atherosclerosis ',NULL,'SCA'),
+	 (19,'Coronavirus disease of 2019 ','Fewer','COVID-19'),
+	 (20,'Precancerous lesions of gastric cancer',NULL,'PLGC');
+INSERT INTO microbiome.`condition` (condition_id,condition_type,complication_or_side_disease,abbreviation) VALUES
+	 (21,'Kwashiorkor',NULL,'KWK'),
+	 (22,'Inflammatory bowel disease ',NULL,'IBD'),
+	 (23,'Hyperlipidemia',NULL,'HL'),
+	 (24,'Early alcoholic steatohepatitis',NULL,'EAS'),
+	 (25,'DSS-induced acute colitis',NULL,'DDS colitis'),
+	 (26,'Nonalcoholic fatty liver disease',NULL,'NAFLD'),
+	 (27,'Parkinson’s disease',NULL,'PD'),
+	 (28,'Type 1 diabetes',NULL,'T1D'),
+	 (29,'Multiple islet autoantibodies',NULL,'≥2IA'),
+	 (30,'Prenatal stress',NULL,'PS');
---
+++ Desktop/host_202311141040.sql
@@ -0,0 +1,91 @@
+INSERT INTO microbiome.host (host_id,host_type,species,strain,race,geographic_location,test_sample_size,control_group,control_sample_size,period,industrialization,recent_antibiotic_use,recent_probiotic_or_prebiotic_use,lifestyle,diet,site_id,condition_id,intervention) VALUES
+	 (1,'Human','Homo sapiens',NULL,NULL,'Indonesia','14','Healthy controls','14','modern','industrial',NULL,'NA',NULL,'5-week no yogurt diet','1','1',NULL),
+	 (2,'Human','Homo sapiens',NULL,NULL,'UK','18','Same subjects at baseline','18','modern','industrial','No','No',NULL,'7-day low FODMAP diet (7g twice daily of maltodextrin)','1',NULL,NULL),
+	 (3,'Human','Homo sapiens',NULL,NULL,'UK','19','Same subjects at baseline','19','modern','industrial','No','No',NULL,'7-day low FODMAP diet (7g twice daily of oligofructose)','2',NULL,NULL),
+	 (4,'Non-human','Mus musculus','C57BL/6',NULL,'USA','7','Control diet','7','modern',NULL,NULL,'NA',NULL,'8-week tryptophan-low diet ','1','2',NULL),
+	 (5,'Non-human','Mus musculus','C57BL/6',NULL,'USA','7','Control diet','7','modern',NULL,NULL,'NA',NULL,'8-week tryptophan-high diet ','2','2',NULL),
+	 (6,'Human','Homo sapiens',NULL,NULL,'India','25','Healthy controls','30','modern','industrial','No','No',NULL,NULL,'1','3',NULL),
+	 (7,'Human','Homo sapiens',NULL,NULL,'India','28','Healthy controls','30','modern','industrial','No','No',NULL,NULL,'1','4',NULL),
+	 (8,'Human','Homo sapiens',NULL,NULL,'Spain','95','Normal mucosal samples','95','modern','industrial',NULL,'NA',NULL,NULL,'2','1',NULL),
+	 (9,'Human','Homo sapiens',NULL,NULL,'["Germany", "England"]','48','Normal mucosal samples','48','modern','industrial',NULL,'NA',NULL,NULL,'2','1',NULL),
+	 (10,'Human','Homo sapiens',NULL,NULL,'USA','44','Normal mucosal samples','44','modern','industrial',NULL,'NA',NULL,NULL,'2','1',NULL);
+INSERT INTO microbiome.host (host_id,host_type,species,strain,race,geographic_location,test_sample_size,control_group,control_sample_size,period,industrialization,recent_antibiotic_use,recent_probiotic_or_prebiotic_use,lifestyle,diet,site_id,condition_id,intervention) VALUES
+	 (11,'Human','Homo sapiens',NULL,NULL,'USA','14','Healthy controls','22','modern','industrial',NULL,'NA',NULL,'3-day dietary survey','1','8',NULL),
+	 (12,'Human','Homo sapiens',NULL,NULL,'China','19','Healthy controls','20','modern','industrial','No','No',NULL,NULL,'1','9',NULL),
+	 (13,'Human','Homo sapiens',NULL,NULL,'South Korea','17','Normal-weight','24','modern','industrial','No','No','8-week weight reduction program','8-week weight reduction program','1','6',NULL),
+	 (14,'Human','Homo sapiens',NULL,NULL,'China','65','Normal mucosal samples','65','modern','industrial','No','No',NULL,NULL,'2','1',NULL),
+	 (15,'Human','Homo sapiens',NULL,NULL,'USA','44','Normal mucosal samples','44','modern','industrial',NULL,'NA',NULL,NULL,'2','1',NULL),
+	 (16,'Non-human','Rattus norvegicus','Wista rat',NULL,'France','26','Chow diet ','7','modern',NULL,NULL,'NA',NULL,'4-day western diet','2',NULL,NULL),
+	 (17,'Non-human','Mus musculus','C57BL/6',NULL,'China','7','Standard control diet ','7','modern',NULL,NULL,'NA',NULL,'8-week western diet','2','7',NULL),
+	 (18,'Human','Homo sapiens',NULL,NULL,'USA ','126','Healthy controls','38','modern','industrial','No','No','NA ','7-day western diet','1','10',NULL),
+	 (19,'Human','Homo sapiens',NULL,NULL,'USA','17','Control subjects','12','modern','industrial',NULL,'NA',NULL,NULL,'1','11','Cardiopulmonary bypass with following antibiotic use'),
+	 (20,'Human','Homo sapiens',NULL,NULL,'Italy','7','Non-dysplastic Barrett’s esophagus group','57','modern','industrial','No','No','WCRF/AICR recommendations (questionnaire)','WCRF/AICR recommendations (questionnaire)','2','12',NULL);
+INSERT INTO microbiome.host (host_id,host_type,species,strain,race,geographic_location,test_sample_size,control_group,control_sample_size,period,industrialization,recent_antibiotic_use,recent_probiotic_or_prebiotic_use,lifestyle,diet,site_id,condition_id,intervention) VALUES
+	 (21,'Human','Homo sapiens',NULL,'Asian','China','158','Control subjects','138','modern','industrial','No','No',NULL,'7-day no yogurt diet','1','6',NULL),
+	 (22,'Human','Homo sapiens',NULL,'Caucasian','Italy','42','Normal‐weight children','36','modern','industrial','No','No',NULL,'Questionnaire','1','6',NULL),
+	 (23,'Human','Homo sapiens',NULL,NULL,'Saudi Arabia','17','Healthy controls','18','modern','industrial',NULL,'NA',NULL,NULL,'1','9',NULL),
+	 (24,'Human','Homo sapiens',NULL,NULL,'Saudi Arabia','17','Healthy controls','18','modern','industrial',NULL,'NA',NULL,NULL,'2','9',NULL),
+	 (25,'Human','Homo sapiens',NULL,NULL,'Italy','34','Paired normal tissue','34','modern','industrial',NULL,'NA',NULL,NULL,'3','13',NULL),
+	 (26,'Human','Homo sapiens',NULL,'["Asian", "Caucasian", "African American"]','USA','11','Healthy controls','10','modern','industrial',NULL,'NA','Questionnaire',NULL,'4','14',NULL),
+	 (27,'Human','Homo sapiens',NULL,NULL,'Germany','75','Same subjects at baseline','75','modern','industrial',NULL,'NA','Questionnaire','Questionnaire','1',NULL,NULL),
+	 (28,'Human','Homo sapiens',NULL,NULL,'South Korea','4','Healthy controls','10','modern','industrial',NULL,'NA',NULL,NULL,'1','15',NULL),
+	 (29,'Human','Homo sapiens',NULL,NULL,'South Korea','3','Healthy controls','10','modern','industrial',NULL,'NA',NULL,NULL,'1','9',NULL),
+	 (30,'Human','Homo sapiens',NULL,NULL,'South Korea','4','Healthy controls','10','modern','industrial',NULL,'NA',NULL,NULL,'1','16',NULL);
+INSERT INTO microbiome.host (host_id,host_type,species,strain,race,geographic_location,test_sample_size,control_group,control_sample_size,period,industrialization,recent_antibiotic_use,recent_probiotic_or_prebiotic_use,lifestyle,diet,site_id,condition_id,intervention) VALUES
+	 (31,'Human','Homo sapiens',NULL,NULL,'Senegal','10','Healthy controls','11','modern','industrial',NULL,'NA',NULL,'Severe acute malnutrition','1',NULL,NULL),
+	 (32,'Human','Homo sapiens',NULL,NULL,'Niger ','10','Healthy controls','9','modern','industrial',NULL,'NA',NULL,'Severe acute malnutrition','1',NULL,NULL),
+	 (33,'Non-human','Mus musculus','B6129SF2',NULL,'USA','10','H2O administration','10','modern',NULL,NULL,'NA',NULL,'20-week alcohol (20%) administration, standard rodent chow','1',NULL,NULL),
+	 (34,'Non-human','Mus musculus','C57BL/6J',NULL,'Japan','5','Normal chow diet','5','modern',NULL,NULL,'NA',NULL,'4-week high‐fat/high‐sucrose diet','1',NULL,NULL),
+	 (35,'Non-human','Mus musculus','Cyp2a12−/−Cyp2c70−/− DKO',NULL,'Japan','5','Normal chow diet','5','modern',NULL,NULL,'NA',NULL,'4-week high‐fat/high‐sucrose diet','1',NULL,NULL),
+	 (36,'Human','Homo sapiens',NULL,NULL,'Italy','144','Healthy controls','201','modern','industrial',NULL,'NA','Questionnaire','Questionnaire','1','18',NULL),
+	 (37,'Human','Homo sapiens',NULL,NULL,'China','127','Non-fewer subjects','60','modern','industrial',NULL,'NA',NULL,'Wuhan Union Hospital diet','1','19',NULL),
+	 (38,'Non-human','Rattus norvegicus','Wista rat',NULL,'China','9','Healthy controls','9','modern',NULL,NULL,'NA',NULL,'32-week 120 μg/ml of MNNG aqueous solution and granular SPF-grade fodder containing 0.05% ranitidine','1','20',NULL),
+	 (39,'Non-human','Mus musculus','BKS.Cg-Dock7m +/+ Leprdb/J',NULL,'USA','7','Healthy controls','7','modern',NULL,NULL,'NA',NULL,'Rodent chow','2','4',NULL),
+	 (40,'Non-human','Rattus norvegicus','Wista rat',NULL,'Spain','3','Normal chow diet','3','modern',NULL,NULL,'NA',NULL,'65% food restriction of a normal chow-diet (2.9 kcal/g; 12% fat, 60%
+carbohydrates and 28% protein) during the last third of gestation and postnatally','1',NULL,NULL);
+INSERT INTO microbiome.host (host_id,host_type,species,strain,race,geographic_location,test_sample_size,control_group,control_sample_size,period,industrialization,recent_antibiotic_use,recent_probiotic_or_prebiotic_use,lifestyle,diet,site_id,condition_id,intervention) VALUES
+	 (41,'Human','Homo sapiens',NULL,NULL,'["Senegal", "Niger"]','10','Healthy controls','5','modern','industrial',NULL,'NA',NULL,NULL,'1','21',NULL),
+	 (42,'Human','Homo sapiens',NULL,NULL,'France','235','Healthy controls','38','modern','industrial',NULL,'NA',NULL,NULL,'1','22',NULL),
+	 (43,'Human','Homo sapiens',NULL,NULL,'Italy','15','Healthy controls','17','modern','industrial','No','No',NULL,'Cardiovascular health integrated lifestyle diet','1','23',NULL),
+	 (44,'Non-human','Mus musculus','C57BL/6',NULL,'USA','9','Pair-fed animals','7','modern',NULL,NULL,'NA',NULL,'10-day Lieber-DeCarli ethanol diet','2','24',NULL),
+	 (45,'Non-human','Mus musculus','C57BL/6',NULL,'Canada','8–10','Control diet ','8–10','modern',NULL,NULL,'NA',NULL,'4-week heme-supplemented diet with 50 mg/kg iron in the form of hemin','1',NULL,NULL),
+	 (46,'Non-human','Mus musculus','C57BL/6',NULL,'Canada','8–10','Control diet ','8–10','modern',NULL,NULL,'NA',NULL,'10-day control diet and administration of DSS (0.75% w/v) in drinking water','1','25',NULL),
+	 (47,'Non-human','Mus musculus','C57BL/6',NULL,'China','6','Normal air exposure','6','modern',NULL,NULL,'NA','Cigarette smoke exposure','Normal chow diet','1',NULL,NULL),
+	 (48,'Non-human','Mus musculus','C57BL/6',NULL,'China','6','Normal air exposure','6','modern',NULL,NULL,'NA','Cigarette smoke exposure','Normal chow diet','2',NULL,NULL),
+	 (49,'Non-human','Rattus norvegicus','Wista rat',NULL,'China','10','Normal-protein diet','10','modern',NULL,NULL,'NA',NULL,'6-week high-protein diet (45% protein, 30% carbohydrate)','1',NULL,NULL),
+	 (50,'Non-human','Rattus norvegicus','Sprague Dawley rat',NULL,'USA','32','No sugar diet','10','modern',NULL,NULL,'NA',NULL,'11% wt:vo sugar diet and Lab Diet 5001','1',NULL,NULL);
+INSERT INTO microbiome.host (host_id,host_type,species,strain,race,geographic_location,test_sample_size,control_group,control_sample_size,period,industrialization,recent_antibiotic_use,recent_probiotic_or_prebiotic_use,lifestyle,diet,site_id,condition_id,intervention) VALUES
+	 (51,'Human','Homo sapiens',NULL,NULL,'Finland','18','Normal-weight subjects','36','modern','industrial',NULL,'NA','pregnancy ',NULL,'1','6',NULL),
+	 (52,'Human','Homo sapiens',NULL,NULL,'South Korea','76','Healthy controls','192','modern','industrial','No','No',NULL,NULL,'1','26',NULL),
+	 (53,'Human','Homo sapiens',NULL,NULL,'South Korea','76','Healthy controls','192','modern','industrial','No','No',NULL,NULL,'5','26',NULL),
+	 (54,'Human','Homo sapiens',NULL,NULL,'Japan','205','Healthy controls','669','modern','industrial',NULL,'NA',NULL,NULL,'1','26',NULL),
+	 (55,'Human','Homo sapiens',NULL,'Asian','China','51','Healthy controls','48','modern','industrial','No','No','Reqruiting healthy spouses','Questionnaire','1','27',NULL),
+	 (56,'Human','Homo sapiens',NULL,NULL,'Australia','29','Healthy controls','41','modern','industrial','No','No','Reqruiting healthy siblings and friends','Food frequency questionnaire','1','28',NULL),
+	 (57,'Human','Homo sapiens',NULL,NULL,'Australia','12','Healthy controls','41','modern','industrial','No','No','Reqruiting healthy siblings and friends','Food frequency questionnaire','1','29',NULL),
+	 (58,'Human','Homo sapiens',NULL,NULL,'USA','20','Paired normal tissue','20','modern','industrial',NULL,'NA',NULL,NULL,'3','13',NULL),
+	 (59,'Non-human','Mus musculus','C57BL/6',NULL,'USA','9* maximum of 3 pups per dam','No stress','7* maximum of 3 pups per dam','modern',NULL,NULL,'NA','Behavioral testing','Food and water ad libitum','1','30',NULL),
+	 (60,'Human','Homo sapiens',NULL,'Caucasian','Australia','1413','Early farming period','28','modern','industrial',NULL,'NA','Modern ','Modern','6',NULL,NULL);
+INSERT INTO microbiome.host (host_id,host_type,species,strain,race,geographic_location,test_sample_size,control_group,control_sample_size,period,industrialization,recent_antibiotic_use,recent_probiotic_or_prebiotic_use,lifestyle,diet,site_id,condition_id,intervention) VALUES
+	 (61,'Human','Homo sapiens',NULL,NULL,'["Germany", "England"]','28','Mesolithic hunter-gatherer period','6','farming','non-industrial',NULL,'NA','Early farming','Early farming','6',NULL,NULL),
+	 (62,'Human','Homo sapiens',NULL,NULL,'Italy','15','Burkina Faso regular diet','14','modern','industrial','No','No',NULL,'Regular urban diet in Italy','1',NULL,NULL),
+	 (63,'Human','Homo sapiens',NULL,NULL,'NA','6874','Non-western-lifestyle population','272','modern','industrial',NULL,'NA','Western-lifestyle populations','Western-lifestyle populations','1',NULL,NULL),
+	 (64,'Human','Homo sapiens',NULL,NULL,'["UK", "Australia"]','6','1918 mummy','1','modern','industrial',NULL,'NA',NULL,NULL,'1',NULL,NULL),
+	 (65,'Human','Homo sapiens',NULL,NULL,'["UK", "Australia"]','6','Iceman mummy','1','modern','industrial',NULL,'NA',NULL,NULL,'1',NULL,NULL),
+	 (66,'Human','Homo sapiens',NULL,NULL,'NA','170','Palaeolithic hunter-gatherer period','21','modern','industrial',NULL,'NA',NULL,NULL,'7',NULL,NULL),
+	 (67,'Human','Homo sapiens',NULL,NULL,'["Fiji", "Peru", "Madagascar", "Tanzania", "Mexico"]','371','Palaeofaeces','8','modern','non-industrial',NULL,'NA','More active lifestyle','Unprocessed and self-produced foods','1',NULL,NULL),
+	 (68,'Human','Homo sapiens',NULL,NULL,'["USA", "Denmark", "Spain"]','418','Palaeofaeces','8','modern','industrial',NULL,'NA','Sedentary lifestyle','Western diet','1',NULL,NULL),
+	 (69,'Human','Homo sapiens',NULL,NULL,'["USA", "Denmark", "Spain"]','418','Non-industrial modern subjects','370','modern','industrial',NULL,'NA','Sedentary lifestyle','Western diet','1',NULL,NULL),
+	 (70,'Human','Homo sapiens',NULL,NULL,'Amazonia','5','Huecoids, Saladoids','15','modern','non-industrial',NULL,'NA',NULL,NULL,'1',NULL,NULL);
+INSERT INTO microbiome.host (host_id,host_type,species,strain,race,geographic_location,test_sample_size,control_group,control_sample_size,period,industrialization,recent_antibiotic_use,recent_probiotic_or_prebiotic_use,lifestyle,diet,site_id,condition_id,intervention) VALUES
+	 (71,'Human','Homo sapiens',NULL,NULL,'Spain','25','Medieval German','NA','modern','industrial',NULL,'NA',NULL,'Questionnaire','8',NULL,NULL),
+	 (72,'Human','Homo sapiens',NULL,NULL,'Spain','25','Industiral England','NA','modern','industrial',NULL,'NA',NULL,'Questionnaire','8',NULL,NULL),
+	 (73,'Human','Homo sapiens',NULL,NULL,'Spain','25','Neanderthal','NA','modern','industrial',NULL,'NA',NULL,'Questionnaire','8',NULL,NULL),
+	 (74,'Human','Homo sapiens',NULL,NULL,'Spain','25','Medieval England (St Mary Graces)','NA','modern','industrial',NULL,'NA',NULL,'Questionnaire','8',NULL,NULL),
+	 (75,'Human','Homo sapiens',NULL,NULL,'["China", "USA", "Australia"]','287','Historical dental calculus ','110','modern','industrial',NULL,'NA',NULL,NULL,'8',NULL,NULL),
+	 (76,'Human','Homo sapiens',NULL,NULL,'["Japan", "Philippines", "USA", "China", "Denmark", "Fiji"]','387','Historical dental calculus ','110','modern','industrial',NULL,'NA',NULL,NULL,'9',NULL,NULL),
+	 (77,'Human','Homo sapiens',NULL,NULL,'Germany','144','Ordinary omnivorous diet ','249','modern','industrial',NULL,'NA',NULL,'Vegetarian diet','1',NULL,NULL),
+	 (78,'Human','Homo sapiens',NULL,NULL,'Germany','105','Ordinary omnivorous diet ','249','modern','industrial',NULL,'NA',NULL,'Strict vegan diet','1',NULL,NULL),
+	 (79,'Human','Homo sapiens',NULL,NULL,'USA','3','Hinds Cave (∼8000 years B.P.) in the southern United States','2','modern','industrial',NULL,'NA',NULL,NULL,'1',NULL,NULL),
+	 (80,'Human','Homo sapiens',NULL,NULL,'USA','3','Rio Zape in northern Mexico (1400 years B.P.)','2','modern','industrial',NULL,'NA',NULL,NULL,'1',NULL,NULL);
+INSERT INTO microbiome.host (host_id,host_type,species,strain,race,geographic_location,test_sample_size,control_group,control_sample_size,period,industrialization,recent_antibiotic_use,recent_probiotic_or_prebiotic_use,lifestyle,diet,site_id,condition_id,intervention) VALUES
+	 (81,'Human','Homo sapiens',NULL,NULL,'USA','3','Caserones (1600 years B.P.) in northern Chile','3','modern','industrial',NULL,'NA',NULL,NULL,'1',NULL,NULL);
